7d7946c9e01e8866e6ce87dd8653db3d236397d2
rajbarmaiya08 <106380134+rajbarmaiya08@users.noreply.github.com>
2022-09-15 19:38:59 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -252,7 +252,7 @@ def start(update, context):
     else:
         buttons.buildbutton(f"{START_BTN1_NAME}", f"{START_BTN1_URL}")
         buttons.buildbutton(f"{START_BTN2_NAME}", f"{START_BTN2_URL}")
-    reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
+#     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''This bot can mirror all your links to Google Drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
